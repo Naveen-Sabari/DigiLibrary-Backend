7ed1e5c572f82b8ad5918e28519f65886d9df7d6
Naveen-Sabari <itznaveen1402@gmail.com>
2024-11-14 17:20:58 +0530

    app1

--- app.js
+++ app.js
@@ -13,33 +13,37 @@ const cloudinary = require('cloudinary').v2;
 const bcrypt = require('bcryptjs');
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME, 
+  cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET
 });
 
 const app = express();
 
-
 dotenv.config({ path: path.join(__dirname, 'config', 'config.env') });
 
+// Configure CORS dynamically based on environment
+const allowedOrigins = [
+  process.env.CLIENT_URL_DEV,  // For local development
+  process.env.CLIENT_URL_PROD  // For production on Netlify
+];
 
 app.use(cors({
-  origin: 'http://localhost:4200',  
+  origin: allowedOrigins,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,  // Allow cookies to be sent with requests
 }));
-app.use(express.json()); 
+
+app.use(express.json());
 
 connectDatabase();
 
-
-// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-app.use('/api/v1', authRoutes);           
-app.use('/api/v1', authLoginRoutes);      
-app.use('/api/v1/products', products);         
-app.use('/api/v1/orders', orders);            
-app.use('/api/v1/users', userRoutes);         
-
+app.use('/api/v1', authRoutes);
+app.use('/api/v1', authLoginRoutes);
+app.use('/api/v1/products', products);
+app.use('/api/v1/orders', orders);
+app.use('/api/v1/users', userRoutes);
 
 app.use('*', (req, res) => {
   res.status(404).json({ message: 'Route not found' });
@@ -49,7 +53,6 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Server error' });
 });
-
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode`);
